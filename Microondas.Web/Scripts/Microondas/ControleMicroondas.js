d822889d1e4eba9a4c3475d3ef3678067ad67615
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2023-04-09 21:16:05 -0300

    quase funcionando

---
+++ Microondas.Web/Scripts/Microondas/ControleMicroondas.js
@@ -0,0 +1,91 @@
+﻿let interval = 0;
+let remainingSeconds = 0;
+let power = 10;
+let isPaused = false;
+
+let selectedField = document.getElementById("time-input");
+
+function updateProgressLabel() {
+    if (!isPaused) {
+        if (remainingSeconds > 0) {
+            $("#progress-label").text("");
+            let dotsPerSecond = 2 * power;
+            let dots = ".".repeat(Math.ceil((120 - remainingSeconds) * dotsPerSecond / 60));
+            let progressString = dots.padEnd(dots.length + remainingSeconds * dotsPerSecond / 60, " ");
+            $("#progress-label").text(progressString);
+            remainingSeconds--;
+            console.log(remainingSeconds);
+            setTimeout(updateProgressLabel, 1000);
+        } else {
+            stopInterval();
+        }
+    }
+}
+
+function stopInterval() {
+    if (isPaused) {
+        interval = 0;
+        remainingSeconds = 0;
+        isPaused = false;
+    }
+    else
+        isPaused = true;
+}
+
+function startInterval(fastStart) {
+    if (!isPaused) {
+        if (interval !== null && fastStart) {
+            stopInterval();
+            remainingSeconds += 30;
+        } else {
+            if ($("#time-input").val().match(/^\d+(:\d+)?$/) === null) {
+                alert("Tempo inválido. Informe um número inteiro ou no formato de minutos:segundos.");
+                return;
+            }
+            let timeParts = $("#time-input").val().split(":");
+            if (timeParts.length === 1) {
+                remainingSeconds = parseInt(timeParts[0]);
+            } else {
+                remainingSeconds = parseInt(timeParts[0]) * 60 + parseInt(timeParts[1]);
+            }
+            if (remainingSeconds < 1 || remainingSeconds > 120) {
+                alert("Tempo inválido. Informe um tempo entre 1 segundo e 2 minutos.");
+                return;
+            }
+            power = parseInt($("#power-input").val());
+            if (power < 1 || power > 10) {
+                alert("Potência inválida");
+                return;
+            }
+        }
+    }
+    else {
+        isPaused = false;
+    }
+    updateProgressLabel();
+}
+
+function thisOnFocus(field) {
+    selectedField = field;
+}
+
+function inputValue(number) {
+    if (selectedField.id == "time-input" && selectedField.value.length <= 4) {
+        selectedField.value = selectedField.value + number;
+        formatTime();
+    }
+    else if (selectedField.id == "power-input") {
+        selectedField.value = selectedField.value + number;
+        if (selectedField.value < 0)
+            selectedField.value = 0;
+        else if (selectedField.value > 10)
+            selectedField.value = 10;
+    }
+};
+
+function formatTime() {
+    let valor = selectedField.value;
+    valor = valor.replace(/\D/g, ''); // Remove tudo que não é dígito
+    valor = valor.replace(/^(\d{2})(\d)/g, '$1:$2'); // Coloca ':' após os 2 primeiros dígitos
+    selectedField.value = valor;
+}
